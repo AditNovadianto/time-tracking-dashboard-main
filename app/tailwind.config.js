0103d96f163626b2d7a286c2b8243de7b41d01f4
AditNovadianto <aditya.novadianto@gmail.com>
2022-05-03 21:34:53 +0700

    membuat git

---
+++ app/tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: ["./src/**/*.jsx"],
+  theme: {
+    extend: {
+      colors: {
+        "blue-background": "hsl(246, 80%, 60%)",
+        "Light-red-(work)": "hsl(15, 100%, 70%)",
+        "Soft-blue-(play)": "hsl(195, 74%, 62%)",
+        "Light-red-(study)": "hsl(348, 100%, 68%)",
+        "Lime-green-(exercise)": "hsl(145, 58%, 55%)",
+        "Violet-(social)": "hsl(264, 64%, 52%)",
+        "Soft-orange-(self-care)": "hsl(43, 84%, 65%)",
+        "Very-dark-blue": "hsl(226, 43%, 10%)",
+        "Dark-blue": "hsl(235, 46%, 20%)",
+        "Desaturated-blue": "hsl(235, 45%, 61%)",
+        "Pale-Blue": "hsl(236, 100%, 87%)",
+      },
+      fontFamily: {
+        Rubik: "'Rubik', sans-serif",
+      },
+      fill: {
+        purple: "#BBC0FF"
+      }
+    },
+  },
+  plugins: [],
+};
